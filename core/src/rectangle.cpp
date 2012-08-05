a18038b2f745cc09e9df1c429fb722ab5cf4e8ae
Jeff Graves <jgraves@decultured.com>
2012-08-05 02:39:22 -0500

    Cleaning up the project, lots of old unused stuff

--- core/src/rectangle.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  rectangle.cpp
-//  glOMP
-//
-//  Created by Jeffrey Graves on 7/1/12.
-//  Copyright (c) 2012 Decultured. All rights reserved.
-//
-
-#include <iostream>
--- core/src/util/lua_logging.cpp
+++
@@ -1,7 +0,0 @@
-//
-//  lua_logging.cpp
-//  glOMP
-//
-//  Created by Jeffrey Graves on 6/26/12.
-//  Copyright (c) 2012 Decultured. All rights reserved.
-//
